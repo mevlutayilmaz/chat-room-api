4772b449c59b38a45a02f27e655c1c601ee50c20
Mevlüt Akif Yılmaz <yilmaz.akif2525@gmail.com>
2024-12-17 11:47:42 +0300

    ...

--- ChatRoomUI/chat-room-ui/src/api/users.js
+++
@@ -1,9 +0,0 @@
-import api from "./axiosConfig";
-
-export const getAllUsers = async () => {
-  try {
-    return await api.get("/users/getAllUsers", { requiresAuth: true });
-  } catch (error) {
-    console.error('API isteği başarısız:', error);  
-  }
-}
